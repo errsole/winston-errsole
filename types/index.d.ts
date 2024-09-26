0427e1d94cfc4411cec6b2ce0523f84f3e85705d
Saikumar <sai@errsole.com>
2024-09-26 12:14:44 +0530

    feat(ErrsoleWinston): introduce TypeScript definition for ErrsoleWinston

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+declare module 'errsole-winston' {
+  import TransportStream from 'winston-transport';
+
+  interface Log {
+    level: string;
+    message: string;
+    meta?: Record<string, any>;
+  }
+
+  class ErrsoleWinston extends TransportStream {
+    private levelMapping: Record<string, string>;  
+
+    constructor(options?: TransportStream.TransportStreamOptions); 
+
+    /**
+     * Core logging method exposed to Winston.
+     * @param logInfo - Log information including level, message, and metadata.
+     * @param callback - Optional callback function.
+     */
+    log(logInfo: Log, callback?: () => void): void;
+  }
+
+  export default ErrsoleWinston;
+}
